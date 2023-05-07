2f7f0c0c9a3189300e69be10a079a25b91588bca
RafikHachana <rafikhachana@gmail.com>
2023-05-07 15:06:38 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ git clone --branch=main https://github.com/magenta/mt3
 # cd mt3
 # cd mt3 && 
 # mv mt3 mt3_tmp; pwd && mv mt3_tmp/* .; pwd && rm -r mt3_tmp
-exit
+# exit
 # TODO(iansimon): remove pinned numba/llvmlite/ddsp after ddsp updated
 python3 -m pip install nest-asyncio numba==0.56.4 llvmlite==0.39.1 pyfluidsynth==1.3.0 -e .
 python3 -m pip install --no-dependencies --upgrade ddsp
