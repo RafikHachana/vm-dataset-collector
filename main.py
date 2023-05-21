7ed2adc2abfc5d122980aa8e4945665b5dc972d5
RafikHachana <rafikhachana@gmail.com>
2023-05-21 15:50:13 +0300

    add args to main script

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import utils
 import concurrent.futures
+import argparse
+
+args = argparse.ArgumentParser()
+
+args.add_argument("output_dir", default="./data/videos")
+args.add_argument("max_files", type=int, default=10)
+args_parsed = args.parse_args()
 
 # for youtube_id, _ in zip(utils.youtube_ids(), range(10)):
 #     print(youtube_id)
 
 with concurrent.futures.ProcessPoolExecutor(max_workers=100) as executor:
     futures = []
-    for youtube_url, _ in zip(utils.youtube_ids(), range(20)):
+    for youtube_url, _ in zip(utils.youtube_ids(), range(args_parsed.max_files)):
         print(youtube_url)
-        futures.append(executor.submit(utils.download_video, youtube_url=youtube_url))
+        futures.append(executor.submit(utils.download_video, youtube_url=youtube_url, output_dir=args_parsed.output_dir))
         # break
 
     for future in concurrent.futures.as_completed(futures):
